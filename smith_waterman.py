77ca4511519a44cc3a0c5baa9ee3a1185fca6683
robinanwyl <robin.anwyl@gmail.com>
2024-12-26 00:51:23 -0800

    Added code to handle edge cases such as empty sequences and sequences with no alignment. Currently working on handling gaps in given sequences.

--- smith_waterman.py
+++ smith_waterman.py
@@ -24,6 +24,7 @@ class ScoringMatrix:
         scores: NumPy array of scoring matrix
         disp_scores: NumPy array of scoring matrix with directional arrows and
             row/column headings, formatted for display.
+        high_score: Highest score in scoring matrix
         alignments: List of optimal local alignments
         disp_alignments: NumPy array of optimal local alignments, formatted
             for display.
@@ -32,7 +33,8 @@ class ScoringMatrix:
         traceback(): Backtraces through the scoring matrix to find local
             alignments of seq1 and seq2, starting from a given high score cell
         find_all_alignments(): Finds all optimal alignments of seq1 and seq2
-        format_alignments(): Formats optimal alignments for display
+        sequence_alignment(): Calculates scoring matrix, finds all
+            alignments, and formats scoring matrix and alignments for display
     """
     def __init__(self, seq1_, seq2_, match_, mismatch_, gap_penalty_):
         """
@@ -47,8 +49,9 @@ class ScoringMatrix:
         self.scores = np.full((self.nrows, self.ncols), np.nan)
         self.disp_scores = np.full((2*self.nrows + 1, 2*self.ncols + 1),
                                     " ", dtype="<U5")
+        self.high_score = 0
         self.alignments = list()
-        self.disp_alignments = None
+        self.disp_alignments = list()
 
         # Initial setup of self.scores
         self.scores[0, :] = 0 # Populate row 0 with scores of 0
@@ -70,25 +73,26 @@ class ScoringMatrix:
         col3 = [char for _ in range(len1) for char in (0, " ")] + [0]
         self.disp_scores[2, 2:], self.disp_scores[2:, 2] = row3, col3
 
-        # Scoring and display
-        self.score_linear() # Calculate scores
-        self.find_all_alignments() # Determine local optimal alignments
-        self.format_alignments()  # Format alignments for display
-
     def score_linear(self):
         """
         Generates scoring matrix with linear gap penalty. Populates self.scores
         and self.disp_scores.
         """
+        gap_chars = ["-", "_"]
         for i in range(1, self.nrows):
             char1 = self.seq1[i-1]
             for j in range(1, self.ncols):
                 char2 = self.seq2[j-1]
                 # Calculate similarity score between characters
-                if char1 == char2:
+                if char1 == char2: # Match -> match score
                     sim_score = self.match_score
                 else:
-                    sim_score = self.mismatch_score
+                    # Gap in either sequence -> gap penalty
+                    if (char1 in gap_chars or char1.strip() == "" or char2 in
+                            gap_chars or char2.strip() == ""):
+                        sim_score = -1*self.gap_penalty
+                    else: # Two mismatched nucleotides -> mismatch score
+                        sim_score = self.mismatch_score
                 # Calculate potential score of aligning char1 and char2
                 score1 = self.scores[i-1, j-1] + sim_score
                 # Calculate potential score if seq1 has a gap
@@ -109,6 +113,8 @@ class ScoringMatrix:
                     self.disp_scores[2 * i + 2, 2 * j + 1] = "←"
         # Cast self.scores to integer type
         self.scores = self.scores.astype(int)
+        # Record high score
+        self.high_score = np.max(self.scores)
 
     def traceback(self, i, j, align1, align2, aligns, curr_path=None,
                   all_paths=None):
@@ -133,8 +139,9 @@ class ScoringMatrix:
             # Record current path
             all_paths.append(curr_path[:])
             # Record optimal alignment
-            align1, align2 = reversed(align1), reversed(align2)
-            aligns.append(("".join(align1), "".join(align2)))
+            if align1 and align2:
+                align1, align2 = reversed(align1), reversed(align2)
+                aligns.append(("".join(align1), "".join(align2)))
             return
 
         # Mark cell as visited
@@ -168,48 +175,72 @@ class ScoringMatrix:
 
     def find_all_alignments(self):
         """
-        Finds all optimal local alignments from the scoring matrix. Backtraces
-        through the scoring matrix from all high scoring cells.
+        Finds all optimal local alignments from the scoring matrix -
+        backtracks through the scoring matrix from all high scoring cells.
+        Formats alignments for printing.
         """
+        # Find alignments
         all_aligns = list()
-        max_score = np.max(self.scores)
         # Traceback from all cells with max score
         for i in range(self.nrows):
             for j in range(self.ncols):
-                if self.scores[i, j] == max_score:
+                if self.scores[i, j] == self.high_score:
                     self.traceback(i, j, [], [], all_aligns)
         self.alignments = all_aligns
+        print(f"self.alignments = {self.alignments}")
 
-    def format_alignments(self):
-        """
-        Populates self.disp_alignments NumPy array with the optimal local
-        alignments of seq1 and seq2, for display.
-        """
-        pass
+        # Format alignments
         # Get lengths of all sequences in optimal alignments
         all_aligns = [seq for align in self.alignments for seq in align]
+        print(f"all_aligns = {all_aligns}")
         seq_lens = [len(s) for s in all_aligns]
         # Generate matrix to display optimal alignments
-        m_rows = len(all_aligns)+len(self.alignments)-1
+        m_rows = len(all_aligns) + len(self.alignments) - 1
         m_cols = max(seq_lens)
         self.disp_alignments = \
-            np.full((m_rows, m_cols)," ", dtype="<U1")
+            np.full((m_rows, m_cols), " ", dtype="<U1")
         # Assign each sequence to a row of the alignment display matrix
-        row_nums = [i + (i//2) for i in range(m_rows)]
+        row_nums = [i + (i // 2) for i in range(m_rows)]
         row_assign = list(zip(row_nums, all_aligns))
         for seq in row_assign:
             # Add whitespace to end of each sequence, then add to matrix
             self.disp_alignments[seq[0], :] = [_ for _ in seq[1].ljust(m_cols)]
 
+    def sequence_alignment(self):
+        """
+        Populates scoring matrix and formats it for display. Finds all optimal
+        local alignments and formats them for display.
+        """
+        # Error if sequence is empty string, whitespace, or None
+        if (not self.seq1 or not self.seq2 or self.seq1.strip() == "" or
+                self.seq2.strip() == ""):
+            raise ValueError("Error: Empty sequence")
+
+        self.score_linear()
+
+        # Handle case of no optimal alignments
+        if self.high_score == 0:
+            return
+
+        self.find_all_alignments()
+
     def __str__(self):
         df_scores = pd.DataFrame(self.disp_scores)
-        df_align = pd.DataFrame(self.disp_alignments)
-        to_print = \
-            (f"Scoring Matrix\n{df_scores.to_string(index=False, header=False)}" +
-            f"\n\nAlignments\n{df_align.to_string(index=False, header=False)}")
+        if self.alignments:
+            df_align = pd.DataFrame(self.disp_alignments)
+            to_print = \
+                (f"Scoring Matrix\n{df_scores.to_string(index=False, header=False)}"
+                 + f"\n\nAlignments\n{df_align.to_string(index=False, header=False)}")
+        else: # If no alignments, don't print disp_alignments
+            to_print = \
+                (f"Scoring Matrix\n{df_scores.to_string(index=False, header=False)}"
+                + f"\n\nAlignments\n{self.alignments}")
         return to_print
 
 
 # Testing code
-matrix = ScoringMatrix("GGATCGA", "GAATTCAGTTA", 5, -3, 4)
+matrix = ScoringMatrix("AC-GT", "ACG-T", 2, -1, 3)
+matrix.sequence_alignment()
+print(matrix.high_score)
+print(matrix.alignments)
 print(matrix)
