9f3c55f90b646043c572711bb563701aa4fa263b
robinanwyl <robin.anwyl@gmail.com>
2024-12-26 22:13:06 -0800

    Final draft. Tests for complete matches, complete mismatches, sequences with different lengths, empty string sequences, whitespace sequences, and sequences from BioGem.org and Wikipedia.

--- smith_waterman_test.py
+++ smith_waterman_test.py
@@ -9,10 +9,9 @@ import unittest
 from smith_waterman import *
 
 class SmithWatermanTests(unittest.TestCase):
-    # Simple tests
     def testCompleteMatch(self):
         seq1, seq2 = "ACGT", "ACGT"
-        actual = ScoringMatrix(seq1, seq2, 2, -1, 3)
+        actual = SmithWatermanLinear(seq1, seq2, 2, -1, 3)
         actual.sequence_alignment()
         actual_score, actual_aligns = actual.high_score, actual.alignments
         expected_score, expected_aligns = 8, [("ACGT", "ACGT")]
@@ -21,7 +20,7 @@ class SmithWatermanTests(unittest.TestCase):
 
     def testCompleteMismatch(self):
         seq1, seq2 = "AACC", "GGTT"
-        actual = ScoringMatrix(seq1, seq2, 2, -1, 3)
+        actual = SmithWatermanLinear(seq1, seq2, 2, -1, 3)
         actual.sequence_alignment()
         actual_score, actual_aligns = actual.high_score, actual.alignments
         expected_score, expected_aligns = 0, []
@@ -30,7 +29,7 @@ class SmithWatermanTests(unittest.TestCase):
 
     def testDifferentLengths(self):
         seq1, seq2 = "A", "ACGT"
-        actual = ScoringMatrix(seq1, seq2, 2, -1, 3)
+        actual = SmithWatermanLinear(seq1, seq2, 2, -1, 3)
         actual.sequence_alignment()
         actual_score, actual_aligns = actual.high_score, actual.alignments
         expected_score, expected_aligns = 2, [("A", "A")]
@@ -39,22 +38,19 @@ class SmithWatermanTests(unittest.TestCase):
 
     def testEmptyString(self):
         seq1, seq2 = "", "ACGT"
-        actual = ScoringMatrix(seq1, seq2, 2, -1, 3)
+        actual = SmithWatermanLinear(seq1, seq2, 2, -1, 3)
         with self.assertRaises(ValueError):
             actual.sequence_alignment()
 
     def testWhitespace(self):
         seq1, seq2 = "   ", "ACGT"
-        actual = ScoringMatrix(seq1, seq2, 2, -1, 3)
+        actual = SmithWatermanLinear(seq1, seq2, 2, -1, 3)
         with self.assertRaises(ValueError):
             actual.sequence_alignment()
 
-    def testGaps(self):
-        pass
-
     def testSequences1(self): # Sequences from BioGem.org
         seq1, seq2 = "GGATCGA", "GAATTCAGTTA"
-        actual = ScoringMatrix(seq1, seq2, 5, -3, 4)
+        actual = SmithWatermanLinear(seq1, seq2, 5, -3, 4)
         actual.sequence_alignment()
         actual_score, actual_aligns = actual.high_score, actual.alignments
         expected_score = 14
@@ -65,7 +61,7 @@ class SmithWatermanTests(unittest.TestCase):
 
     def testSequences2(self): # Sequences from Wikipedia
         seq1, seq2 = "GGTTGACTA", "TGTTACGG"
-        actual = ScoringMatrix(seq1, seq2, 3, -3, 2)
+        actual = SmithWatermanLinear(seq1, seq2, 3, -3, 2)
         actual.sequence_alignment()
         actual_score, actual_aligns = actual.high_score, actual.alignments
         expected_score, expected_aligns = 13, [('GTTGAC', 'GTT-AC')]
